3e7b300c66b213a53eb4e1676ec7e4be6f02c753
Dekelbd <dekeldbd@gmail.com>
2021-12-14 15:46:43 +0200

    implanting error handling middleware

--- Server/controllers/organization-controller.js
+++ Server/controllers/organization-controller.js
@@ -1,14 +1,16 @@
 const User = require("../models/user-model");
 const Organization = require("../models/organization-model");
+const HttpError = require("../utils/HttpError");
 exports.getUsers = async (req, res, next) => {
   try {
     const organizations = await Organization.findById(
       req.params.organizationId
     );
     if (!organizations) {
-      throw new Error("cant find this user");
+      throw new HttpError("cant find this user", 404);
     }
+    res.status(200).json({});
   } catch (e) {
-    res.status(404).json({ message: e.message });
+    next(e);
   }
 };
