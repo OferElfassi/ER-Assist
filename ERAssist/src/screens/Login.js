55e099112b6dfe741bb5ded4f595e6755b695715
ofer221 <ofer2212@gmail.com>
2021-12-14 12:39:03 +0200

    update signup and login screen

--- ERAssist/src/screens/Login.js
+++ ERAssist/src/screens/Login.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet} from 'react-native';
 import {setMainRoot} from '../navigation';
+import {TextField, Button, View, Text} from 'react-native-ui-lib';
 
 const LoginScreen = () => {
   return (
     <View style={styles.root}>
-      <Button title="Login" color="#710ce3" onPress={() => setMainRoot()} />
+      <View flex paddingH-25 paddingT-120>
+        <Text blue50 text20>
+          Welcome
+        </Text>
+        <TextField text50 placeholder="username" grey10 />
+        <TextField text50 placeholder="password" secureTextEntry grey10 />
+        <View marginT-100 center>
+          <Button
+            text70
+            white
+            background-orange30
+            label="Login"
+            onPress={() => setMainRoot()}
+          />
+        </View>
+      </View>
     </View>
   );
 };
--- ERAssist/src/screens/Signup.js
+++ ERAssist/src/screens/Signup.js
@@ -1,11 +1,95 @@
-import React from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, ScrollView} from 'react-native';
 import {setMainRoot} from '../navigation';
+import {
+  TextField,
+  Button,
+  View,
+  Text,
+  Switch,
+  Picker,
+} from 'react-native-ui-lib';
+
+/**
+ * username
+ * password
+ * confirm password
+ * address
+ * phone
+ * email
+ * role
+ * password
+ */
+
+const options = [
+  {label: 'ichilov', value: 'ichilov'},
+  {label: 'rambam', value: 'rambam'},
+  {label: 'ziv', value: 'ziv'},
+];
 
 const SignupScreen = () => {
+  const [switchVal, setSwitchVal] = useState(false);
   return (
     <View style={styles.root}>
-      <Button title="Signup" color="#710ce3" onPress={() => setMainRoot()} />
+      <View paddingL-10>
+        <ScrollView>
+          {/*<Picker*/}
+          {/*  marginT-20*/}
+          {/*  placeholder="Organization"*/}
+          {/*  value={options[0]}*/}
+          {/*  // onChange={items => this.setState({languages: items})}*/}
+          {/*  // mode={Picker.modes.MULTI}*/}
+          {/*  selectionLimit={3}*/}
+          {/*  // rightIconSource={dropdown}*/}
+          {/*>*/}
+          {/*  {options.map(option => {*/}
+          {/*    <Picker.Item*/}
+          {/*      key={option.value}*/}
+          {/*      value={option}*/}
+          {/*      disabled={false}*/}
+          {/*    />;*/}
+          {/*  })}*/}
+          {/*</Picker>*/}
+          <Text blue50 text20>
+            Signup
+          </Text>
+          <TextField text placeholder="username" grey10 />
+          <TextField text placeholder="password" secureTextEntry grey10 />
+          <TextField
+            text
+            placeholder="confirm password"
+            secureTextEntry
+            grey10
+          />
+          <TextField text placeholder="address" secureTextEntry grey10 />
+          <TextField text placeholder="phone" secureTextEntry grey10 />
+          <TextField text placeholder="email" secureTextEntry grey10 />
+          <View flex row>
+            <Text>Manager</Text>
+            <Switch
+              onColor={'pink'}
+              offColor={'pink'}
+              testID="switch"
+              value={switchVal}
+              onValueChange={(value1: boolean) => {
+                setSwitchVal(!switchVal);
+              }}
+              style={{marginBottom: 20}}
+            />
+            <Text>Reporter</Text>
+          </View>
+
+          <View marginT-100 center>
+            <Button
+              text70
+              white
+              background-orange30
+              label="Sign Up"
+              onPress={() => setMainRoot()}
+            />
+          </View>
+        </ScrollView>
+      </View>
     </View>
   );
 };
@@ -13,8 +97,8 @@ const SignupScreen = () => {
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
     backgroundColor: 'whitesmoke',
   },
 });
